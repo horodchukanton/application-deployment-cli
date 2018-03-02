240da2a5bdd4ece713536b3e38a0e488abc41086
Anykey <antoman1994@gmail.com>
2018-03-03 00:23:45 +0200

    App::Deployment::Tomcat. added dummy methods

--- lib/App/Deployment/Tomcat.pm
+++ lib/App/Deployment/Tomcat.pm
@@ -6,12 +6,71 @@ use parent 'App::Deployment::Base';
 
 =head1 NAME
 
-  Tomcat - 
+  App::Deployment::Tomcat
 
 =head2 SYNOPSIS
 
-  This package  
+  Concrete implementation for managing applications at Apache Tomcat server
 
 =cut
 
 1;
+#**********************************************************
+=head2 deploy($application_path) - dummy for deploy()
+
+  Arguments:
+     $application_path - path to application file
+
+  Returns:
+    boolean
+
+=cut
+#**********************************************************
+sub deploy {
+  return 1;
+}
+
+#**********************************************************
+=head2 start($application_path) - dummy for start()
+
+  Arguments:
+     $application_path - path to application file
+
+  Returns:
+    boolean
+
+=cut
+#**********************************************************
+sub start {
+  return 1;
+}
+
+#**********************************************************
+=head2 check($application_path) - dummy for check()
+
+  Arguments:
+     $application_path - path to application file
+
+  Returns:
+    boolean
+
+=cut
+#**********************************************************
+sub check {
+  return 1;
+}
+
+#**********************************************************
+=head2 undeploy($application_path) - dummy for undeploy()
+
+  Arguments:
+     $application_path - path to application file
+
+  Returns:
+    boolean
+
+=cut
+#**********************************************************
+sub undeploy {
+  return 1;
+}
