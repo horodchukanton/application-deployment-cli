489f00b9acdc4451f577e26cfad662e722dc9fd3
Anykey <antoman1994@gmail.com>
2018-03-02 23:59:07 +0200

    App::Deployment::Base. dummy methods

--- lib/App/Deployment/Base.pm
+++ lib/App/Deployment/Base.pm
@@ -18,14 +18,12 @@ use warnings FATAL => 'all';
 =cut
 #**********************************************************
 sub new {
-  use Data::Dumper;
-
   my $class = shift;
 
   my (%conf) = @_;
 
   my $self = {
-    Name => $conf{server}
+    %conf
   };
 
 
@@ -33,5 +31,90 @@ sub new {
 
   return $self;
 }
+
+#**********************************************************
+=head2 get_name() - returns server plugin name
+
+=cut
+#**********************************************************
+sub get_name {
+  my $self = shift;
+  return $self->{server};
+}
+
+#**********************************************************
+=head2 is_reachable() - checks if application server responds
+
+  Returns:
+    boolean
+
+=cut
+#**********************************************************
+sub is_reachable {
+  my ($self) = @_;
+
+  return 0;
+}
+
+#**********************************************************
+=head2 deploy($application_path) - dummy for deploy()
+
+  Arguments:
+     $application_path - path to application file
+
+  Returns:
+    boolean
+
+=cut
+#**********************************************************
+sub deploy {
+  die "Not implemented\n";
+}
+
+#**********************************************************
+=head2 start($application_path) - dummy for start()
+
+  Arguments:
+     $application_path - path to application file
+
+  Returns:
+    boolean
+
+=cut
+#**********************************************************
+sub start {
+  die "Not implemented\n";
+}
+
+#**********************************************************
+=head2 check($application_path) - dummy for check()
+
+  Arguments:
+     $application_path - path to application file
+
+  Returns:
+    boolean
+
+=cut
+#**********************************************************
+sub check {
+  die "Not implemented\n";
+}
+
+#**********************************************************
+=head2 undeploy($application_path) - dummy for undeploy()
+
+  Arguments:
+     $application_path - path to application file
+
+  Returns:
+    boolean
+
+=cut
+#**********************************************************
+sub undeploy {
+  die "Not implemented\n";
+}
+
 
 1;
