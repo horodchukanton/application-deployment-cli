e2b07acc20aac7d3adcce64dbb73f803acfc925c
Anykey <antoman1994@gmail.com>
2018-03-02 23:36:44 +0200

    Deployment.pm. Using AUTOLOAD to pass method calls

--- lib/App/Deployment.pm
+++ lib/App/Deployment.pm
@@ -8,9 +8,24 @@ use warnings FATAL => 'all';
 
 =head2 SYNOPSIS
 
-  This package
+  This package is an Adapter for application server classes that are located under App::Deployment::* namespace
+
+  For simplicity we use AUTOLOAD to keep interface clear and integral.
+  This gives ability to change concrete implementation later saving backward compatibility
 
 =cut
+
+# Transparent passing function call to underlying plugin
+AUTOLOAD {
+  our $AUTOLOAD;
+
+  my $func_name = $AUTOLOAD;
+  $func_name =~ s/.*:://;
+  my ($self, @args) = @_;
+  die "Call of undefined function $func_name " if (!$self->{Host}->can($func_name));
+
+  return $self->{Host}->$func_name(@args);
+}
 
 #**********************************************************
 =head2 new(%conf)
@@ -26,19 +41,16 @@ use warnings FATAL => 'all';
 #**********************************************************
 sub new{
   my $class = shift;
-
   my (%conf) = @_;
+  die "'server' option is mandatory" unless $conf{server};
 
   my $self = { %conf };
-
   bless( $self, $class );
 
-  die "'server' option is mandatory" unless $self->{server};
-
+  # Load plugin
   eval {
     require "App/Deployment/$self->{server}.pm";
     "App::Deployment::$self->{server}"->import();
-
     $self->{Host} = "App::Deployment::$self->{server}"->new(%conf);
   };
   if ($@){
@@ -48,23 +60,23 @@ sub new{
   return $self;
 };
 
-
 #**********************************************************
-=head2 get_server() - returns server plugin name
+=head2 have_method($method) - checks application server for implemented method
 
   Arguments:
-     -
+    $method - string, method name
 
   Returns:
-
+    boolean
 
 =cut
 #**********************************************************
-sub get_server {
-  my $self = shift;
-
-  return $self->{Host}->{Name};
+sub have_method {
+  my ($self, $method) = @_;
+  return $self->{Host}->can($method);
 }
 
+# Added empty DESTROY because of AUTOLOAD use
+DESTROY {};
 
 1;
